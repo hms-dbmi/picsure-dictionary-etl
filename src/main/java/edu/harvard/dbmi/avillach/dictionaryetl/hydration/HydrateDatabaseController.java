73a3e1e3746c3937ae5aeaf3b601fd3769b5c00f
GeorgeC <gcolon021@gmail.com>
2024-12-09 13:30:46 -0500

    Add endpoint for database hydration with facet option
    This commit introduces a new GET endpoint, '/initialize', in the HydrateDatabaseController to facilitate database hydration. The endpoint allows optional parameters for datasetName, csvPath, errorDirectory, and a boolean to include default facets, enabling more flexible database initialization. Additionally, the FacetService is integrated to support the creation of default facets if specified.

--- src/main/java/edu/harvard/dbmi/avillach/dictionaryetl/hydration/HydrateDatabaseController.java
+++ src/main/java/edu/harvard/dbmi/avillach/dictionaryetl/hydration/HydrateDatabaseController.java
@@ -1,10 +1,37 @@
 package edu.harvard.dbmi.avillach.dictionaryetl.hydration;
 
+import edu.harvard.dbmi.avillach.dictionaryetl.facet.FacetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+@Controller(value = "/hydrate")
 public class HydrateDatabaseController {
 
+    private final HydrateDatabaseService hydrateDatabaseService;
+    private final FacetService facetService;
 
+    @Autowired
+    public HydrateDatabaseController(HydrateDatabaseService hydrateDatabaseService, FacetService facetService) {
+        this.hydrateDatabaseService = hydrateDatabaseService;
+        this.facetService = facetService;
+    }
 
+    @GetMapping(value = "/initialize")
+    public ResponseEntity<String> initialDatabaseHydration(
+            @RequestParam(required = false) String datasetName,
+            @RequestParam(required = false) String csvPath,
+            @RequestParam(required = false) String errorDirectory,
+            @RequestParam(required = false) boolean includeDefaultFacets
+            ) {
+        this.hydrateDatabaseService.processColumnMetaCSV(csvPath, datasetName, errorDirectory);
+        if (includeDefaultFacets) {
+            this.facetService.createDefaultFacets();
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
