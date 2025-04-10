5f973cf01c4bd99d4c849d8326b46debc752b34f
GeorgeC <gcolon021@gmail.com>
2025-04-10 09:12:38 -0400

    Convert DictionaryCSVController to a RestController
    Replaced @Controller annotation with @RestController to simplify API responses and align with RESTful principles. This change ensures consistent behavior for returning JSON responses in the export endpoints.

--- src/main/java/edu/harvard/dbmi/avillach/dictionaryetl/export/DictionaryCSVController.java
+++ src/main/java/edu/harvard/dbmi/avillach/dictionaryetl/export/DictionaryCSVController.java
@@ -3,13 +3,10 @@ package edu.harvard.dbmi.avillach.dictionaryetl.export;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @CrossOrigin(origins = "http://localhost:8081")
-@Controller
+@RestController
 @RequestMapping("export")
 public class DictionaryCSVController {
 
