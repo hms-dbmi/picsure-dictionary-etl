01f87e6b4cd43187c6792fa8b89b20d84d9094ed
GeorgeC <gcolon021@gmail.com>
2025-11-04 10:16:08 -0500

    Add curated facet metadata and scoped request filters to RecoverMonthsFacetGeneratorServiceNestedTest

--- src/test/java/edu/harvard/dbmi/avillach/dictionaryetl/facet/RecoverMonthsFacetGeneratorServiceNestedTest.java
+++ src/test/java/edu/harvard/dbmi/avillach/dictionaryetl/facet/RecoverMonthsFacetGeneratorServiceNestedTest.java
@@ -5,7 +5,9 @@ import edu.harvard.dbmi.avillach.dictionaryetl.concept.ConceptModel;
 import edu.harvard.dbmi.avillach.dictionaryetl.concept.ConceptService;
 import edu.harvard.dbmi.avillach.dictionaryetl.dataset.DatasetModel;
 import edu.harvard.dbmi.avillach.dictionaryetl.dataset.DatasetRepository;
+import edu.harvard.dbmi.avillach.dictionaryetl.facet.model.FacetMetadataModel;
 import edu.harvard.dbmi.avillach.dictionaryetl.facet.model.FacetModel;
+import edu.harvard.dbmi.avillach.dictionaryetl.facetcategory.FacetCategoryModel;
 import edu.harvard.dbmi.avillach.dictionaryetl.facetcategory.FacetCategoryRepository;
 import edu.harvard.dbmi.avillach.dictionaryetl.facet.dto.GenerateRecoverMonthsRequest;
 import edu.harvard.dbmi.avillach.dictionaryetl.facet.dto.GenerateRecoverMonthsResponse;
@@ -46,6 +48,8 @@ class RecoverMonthsFacetGeneratorServiceNestedTest {
     private FacetCategoryRepository categoryRepository;
     @Autowired
     private FacetConceptRepository facetConceptRepository;
+    @Autowired
+    private FacetMetadataRepository facetMetadataRepository;
 
     @Container
     static final PostgreSQLContainer<?> db = new PostgreSQLContainer<>("postgres:16")
@@ -70,24 +74,32 @@ class RecoverMonthsFacetGeneratorServiceNestedTest {
         // Seed datasets
         DatasetModel dsAdult = datasetRepository.save(new DatasetModel("phs003436", "RECOVER Adult", "", ""));
         DatasetModel dsPeds = datasetRepository.save(new DatasetModel("phs003431", "RECOVER Pediatrics", "", ""));
+        FacetCategoryModel consortiumFacetCat = categoryRepository.save(new FacetCategoryModel("Consortium_Curated_Facets", "Consortium_Curated_Facets", null));
+        FacetModel facetRecoverAdult = facetRepository.save(new FacetModel(consortiumFacetCat.getFacetCategoryId(), "RECOVER Adult Curated", "RECOVER Adult Curated", "RECOVER Adult Curated Description", null));
+        facetMetadataRepository.save(new FacetMetadataModel(facetRecoverAdult.getFacetId(), FacetLoaderService.KEY_EFFECTIVE_EXPRESSIONS, "[{ \"exactly\": \"phs003463\", \"node\": 0 },{ \"regex\": \"(?i)RECOVER_Adult$\", \"node\": 1 }]"));
 
         // Seed concepts: two RECOVER Adult with months; one Pediatrics with similar tail (should NOT match)
         ConceptModel a9 = new ConceptModel(dsAdult.getDatasetId(), "phs003436", "phs003436", "",
                 "\\phs003436\\RECOVER_Adult\\biospecimens\\Inventory of Samples Collected\\ac_cptcoll\\Inf\\9\\", null);
         ConceptModel a12 = new ConceptModel(dsAdult.getDatasetId(), "phs003436", "phs003436", "",
-                "\\phs003436\\RECOVER_Adult_v5\\visits\\Noninf\\12\\", null);
+                "\\phs003436\\RECOVER_Adult\\visits\\Noninf\\12\\", null);
         ConceptModel p12 = new ConceptModel(dsPeds.getDatasetId(), "phs003431", "phs003431", "",
                 "\\phs003431\\RECOVER_Pediatrics\\visits\\Inf\\12\\", null);
-        conceptService.save(a9); conceptService.save(a12); conceptService.save(p12);
+        conceptService.save(a9);
+        conceptService.save(a12);
+        conceptService.save(p12);
 
         // Generate (clear category first to ensure clean state)
         GenerateRecoverMonthsRequest req = new GenerateRecoverMonthsRequest();
         req.categoryName = "Consortium_Curated_Facets";
+        req.pathPrefixRegex = "(?i)\\\\phs003436\\\\"; // scope to RECOVER Adult
+        req.adultNodeRegex = "(?i)RECOVER_Adult$";
+        req.studyId = "phs003436";
         GenerateRecoverMonthsResponse resp = service.generate(req);
         assertEquals("Generation complete.", resp.message);
 
         // Parent facet exists in category
-        Optional<FacetModel> parentOpt = facetRepository.findByName("Recover Adult");
+        Optional<FacetModel> parentOpt = facetRepository.findByName("RECOVER Adult Curated");
         assertTrue(parentOpt.isPresent());
         Long parentId = parentOpt.get().getFacetId();
         assertTrue(categoryRepository.findByName("Consortium_Curated_Facets").isPresent());
