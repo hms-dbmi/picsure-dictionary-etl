2ac6038c88491f906b50b8b84ef60906502d1792
GeorgeC <gcolon021@gmail.com>
2025-12-22 16:49:52 -0500

    Removed BDC special case and moved it to curation

--- src/main/java/edu/harvard/dbmi/avillach/dictionaryetl/loading/ColumnMetaTreeBuilder.java
+++ src/main/java/edu/harvard/dbmi/avillach/dictionaryetl/loading/ColumnMetaTreeBuilder.java
@@ -3,7 +3,6 @@ package edu.harvard.dbmi.avillach.dictionaryetl.loading;
 import edu.harvard.dbmi.avillach.dictionaryetl.concept.ConceptModel;
 import edu.harvard.dbmi.avillach.dictionaryetl.concept.ConceptTypes;
 import edu.harvard.dbmi.avillach.dictionaryetl.loading.dto.LoadingContext;
-import edu.harvard.dbmi.avillach.dictionaryetl.loading.dto.LoadingErrorRegistry;
 import edu.harvard.dbmi.avillach.dictionaryetl.loading.model.ColumnMeta;
 import edu.harvard.dbmi.avillach.dictionaryetl.loading.model.ConceptNode;
 import org.apache.commons.lang3.StringUtils;
@@ -11,7 +10,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.concurrent.ConcurrentMap;
-import java.util.regex.Pattern;
 
 @Component
 public class ColumnMetaTreeBuilder {
@@ -74,29 +72,9 @@ public class ColumnMetaTreeBuilder {
             if (i == node.length - 1) {
                 currentNode.getConceptModel().setConceptType(ConceptTypes.conceptTypeFromColumnMeta(columnMeta));
                 currentNode.setConceptMetadataModel(conceptMetadataModelMapper.fromColumnMeta(columnMeta));
-                handleCompliantConceptSpecialCase(currentNode);
             }
 
             parent = currentNode;
-        }
-    }
-
-    /**
-     * If a concept path meets this format: "\\phs\\pht\\^phv[0-9]+$\\variable_name\\"
-     *
-     * Example: \\phs000001\\pht000001\\phv000123\\age\\
-     * The second-to-last node meets the format.
-     *
-     * If a variable meets these criteria, we set the ConceptModel display to the variable name
-     * and Concept name to the phv.
-     *
-     * @param conceptNode Current Concept Node
-     */
-    protected void handleCompliantConceptSpecialCase(ConceptNode conceptNode) {
-        boolean matches = Pattern.matches("^phv[0-9]+$", conceptNode.getParent().getConceptModel().getName());
-        if (matches) {
-            String phvVal = conceptNode.getParent().getConceptModel().getName();
-            conceptNode.getConceptModel().setName(phvVal);
         }
     }
 
